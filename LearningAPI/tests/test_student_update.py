56697f7249f7dc1d2308a28b7f84537ffb20a1a2
Gabe Cziprusz <gcziprusz@gmail.com>
2026-06-03 12:25:44 -0500

    fix test template

--- LearningAPI/tests/test_student_update.py
+++ LearningAPI/tests/test_student_update.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from LearningAPI.models import NssUser
+from rest_framework.test import APIClient
+from pytest import fail
 
 class StudentPartialUpdateTests(APITestCase):
     """Tests for PATCH /students/{pk}/"""
@@ -12,11 +14,12 @@ class StudentPartialUpdateTests(APITestCase):
             password='testpass123'
         )
         nss_user = NssUser.objects.create(
-            user=self.user,
+            user=user,
             slack_handle='@old_handle',
             github_handle='old_github'
         )
         token = Token.objects.create(user=user)
+        client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         """PUT slack_handle should save the new value to the database."""
         response = client.put(
@@ -26,4 +29,4 @@ class StudentPartialUpdateTests(APITestCase):
         )
         #  put your test code here
         # delete this line when you are done
-        fail("Not Implemented yet")
+        fail("Not Yet Implemented")
